3331325f8a5f0c064e096b551d4a690705d948e8
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2024-03-08 07:51:47 +0800

    feat: TBD features

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,104 @@
+import { ReactNode, useCallback, useEffect, useState } from "react";
+
+type Content = {
+  type: string;
+  children: string | string[];
+};
+
+const content: Record<string, Content> = {
+  root: {
+    type: "root",
+    children: ["foo", "bar"],
+  },
+  foo: {
+    type: "header",
+    children: "hi there baby",
+  },
+  bar: {
+    type: "text",
+    children: "hello daddy",
+  },
+};
+
+type Selection = {
+  startId: string | null;
+  endId: string | null;
+  parentId: string | null;
+  startOffset: number | null;
+  endOffset: number | null;
+};
+
+const DEFAULT_SELECTION: Selection = {
+  startId: null,
+  endId: null,
+  parentId: null,
+  startOffset: null,
+  endOffset: null,
+};
+
 function App() {
-  return <h1>hello application</h1>
+  const [selection, setSelection] = useState<Selection>(DEFAULT_SELECTION);
+
+  const handler = useCallback(() => {
+    const selection = window.getSelection();
+    if (!selection) return;
+    const range = selection.getRangeAt(0);
+    setSelection((prevSelection) => ({
+      ...prevSelection,
+      startId: range.startContainer.parentElement?.dataset?.["rteId"] ?? null,
+      endId: range.endContainer.parentElement?.dataset?.["rteId"] ?? null,
+      parentId:
+        (range.commonAncestorContainer as HTMLElement).dataset?.["rteId"] ??
+        null,
+      startOffset: range.startOffset,
+      endOffset: range.endOffset - 1,
+    }));
+  }, []);
+
+  console.log(selection);
+
+  useEffect(() => {
+    document.addEventListener("selectionchange", handler);
+    return () => {
+      document.removeEventListener("selectionchange", handler);
+    };
+  }, []);
+
+  const renderContent = (node: Content, nodeId?: string): ReactNode => {
+    if (typeof node.children !== "string") {
+      return node.children.map((nodeId) =>
+        renderContent(content[nodeId], nodeId),
+      );
+    }
+
+    const commonProps = { key: nodeId, "data-rte-id": nodeId };
+    switch (node.type) {
+      case "header":
+        return <h1 {...commonProps}>{node.children}</h1>;
+      case "text":
+        return <p {...commonProps}>{node.children}</p>;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div
+      style={{ minHeight: "36px" }}
+      tabIndex={0}
+      onKeyDown={(evt) => {
+        evt.preventDefault();
+        console.log("down", evt.key);
+      }}
+      onKeyUp={(evt) => {
+        evt.preventDefault();
+        console.log("up", evt.key);
+      }}
+      data-rte-id="root"
+    >
+      {renderContent(content.root)}
+    </div>
+  );
 }
 
-export default App
+export default App;
