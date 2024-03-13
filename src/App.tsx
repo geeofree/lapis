13ec3eae917f4406dbe0ed8f955e9fd18e40ee84
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2024-03-13 22:21:48 +0800

    chore: sample usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,178 +1,74 @@
-import { ReactNode, useCallback, useEffect, useState } from "react";
+import { LapisContent, LapisProvider, useLapisContext } from "./lapis";
 
-type ChildNode = {
-  type: "primitive" | "node";
-  value: string | number;
-};
-
-type Nodes = {
-  type: string;
-  children: ChildNode[];
-};
-
-const DEFAULT_CONTENT: Record<string, Nodes> = {
+const DEFAULT_CONTENT: LapisContent = {
   root: {
+    id: "root",
     type: "root",
-    children: [
-      { type: "node", value: "foo" },
-      { type: "node", value: "bar" },
-    ],
+    children: [{ type: "node", value: "quas" }],
   },
-  foo: {
+  quas: {
+    id: "quas",
     type: "header",
     children: [
-      { type: "primitive", value: "Woah! " },
-      { type: "primitive", value: "Hi there baby." },
+      { type: "text", value: "Quas " },
+      { type: "node", value: "wex" },
     ],
   },
-  bar: {
-    type: "text",
-    children: [{ type: "primitive", value: "Hey daddy." }],
+  wex: {
+    id: "wex",
+    type: "span",
+    children: [
+      { type: "text", value: "Wex " },
+      { type: "node", value: "exort" },
+    ],
+  },
+  exort: {
+    id: "exort",
+    type: "italic",
+    children: [{ type: "text", value: "the quick brown fox jumps over" }],
   },
 };
 
-type Selection = {
-  startId: string | null;
-  endId: string | null;
-  parentId: string | null;
-  startOffset: number | null;
-  endOffset: number | null;
-  collapsed: boolean;
-};
-
-const DEFAULT_SELECTION: Selection = {
-  startId: null,
-  endId: null,
-  parentId: null,
-  startOffset: null,
-  endOffset: null,
-  collapsed: false,
-};
+function Container() {
+  const { getContainerProps, getContent } = useLapisContext();
+  return (
+    <div {...getContainerProps()} tabIndex={0}>
+      <h1>Hello application!</h1>
+      {getContent()}
+    </div>
+  );
+}
 
 function App() {
-  const [content, setContent] = useState(DEFAULT_CONTENT);
-  const [selection, setSelection] = useState<Selection>(DEFAULT_SELECTION);
-
-  const handler = useCallback(() => {
-    const selection = window.getSelection();
-    if (!selection) return;
-    const range = selection.getRangeAt(0);
-    console.log(selection, range);
-    setSelection((prevSelection) => ({
-      ...prevSelection,
-      startId:
-        range.startContainer.parentElement?.dataset?.["lapisRteId"] ?? null,
-      endId: range.endContainer.parentElement?.dataset?.["lapisRteId"] ?? null,
-      parentId:
-        (range.commonAncestorContainer as HTMLElement).dataset?.[
-        "lapisRteId"
-        ] ?? null,
-      startOffset: range.startOffset,
-      endOffset: range.endOffset - 1,
-      collapsed: range.collapsed,
-    }));
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener("selectionchange", handler);
-    return () => {
-      document.removeEventListener("selectionchange", handler);
-    };
-  }, []);
-
-  const renderContent = (node: Nodes, nodeId?: string): ReactNode => {
-    const commonProps = {
-      key: nodeId,
-      "data-lapis-rte-id": nodeId,
-    };
-
-    switch (node.type) {
-      case "header":
-        return (
-          <h1 {...commonProps}>
-            {node.children.map((childNode, i) =>
-              childNode.type === "node" ? (
-                renderContent(
-                  content[childNode.value],
-                  childNode.value as string,
-                )
-              ) : (
-                <span
-                  key={`${nodeId}.${i}`}
-                  data-lapis-rte-id={`${nodeId}.${i}`}
-                >
-                  {childNode.value}
-                </span>
-              ),
-            )}
-          </h1>
-        );
-      case "text":
-        return (
-          <p {...commonProps}>
-            {node.children.map((childNode, i) =>
-              childNode.type === "node" ? (
-                renderContent(
-                  content[childNode.value],
-                  childNode.value as string,
-                )
-              ) : (
-                <span
-                  key={`${nodeId}.${i}`}
-                  data-lapis-rte-id={`${nodeId}.${i}`}
-                >
-                  {childNode.value}
-                </span>
-              ),
-            )}
-          </p>
-        );
-      case "root":
-        return node.children.map((childNode, i) =>
-          childNode.type === "node" ? (
-            renderContent(content[childNode.value], childNode.value as string)
-          ) : (
-            <span key={`${nodeId}.${i}`} data-lapis-rte-id={`${nodeId}.${i}`}>
-              {childNode.value}
-            </span>
-          ),
-        );
-      default:
-        return null;
-    }
-  };
-
   return (
-    <div
-      style={{ minHeight: "36px" }}
-      tabIndex={0}
-      onKeyDown={(evt) => {
-        evt.preventDefault();
-        if (evt.key.length === 1) {
-          setContent((currentContent) => {
-            const [startId, startIdIndex] = selection.startId?.split(".") ?? [];
-            const [endId, endIdIndex] = selection.endId?.split(".") ?? [];
-
-            const startNode =
-              currentContent[startId].children[Number(startIdIndex)];
-            const endNode = currentContent[endId].children[Number(endIdIndex)];
-            /**
-             * TODO
-             * Implement the content update logic
-             **/
-            console.log(startNode, endNode, selection, evt.key);
-            return currentContent;
-          });
+    <LapisProvider
+      contentRootId="root"
+      defaultContent={DEFAULT_CONTENT}
+      onRender={(type, props) => {
+        switch (type) {
+          case "text":
+            return <p {...props} />;
+          case "span":
+            return <span {...props} />;
+          case "bold":
+            return <b {...props} />;
+          case "italic":
+            return <i {...props} />;
+          case "header":
+            return <h1 {...props} />;
+          default:
+            return null;
         }
       }}
-      onKeyUp={(evt) => {
-        evt.preventDefault();
-        console.log("up", evt.key);
+      onSelection={(content, selection) => {
+        console.log("geo-selection", content, selection);
       }}
-      data-rte-id="root"
+      onKeyDown={(evt, content, selection) => {
+        console.log(evt.key, content, selection);
+      }}
     >
-      {renderContent(content.root, "root")}
-    </div>
+      <Container />
+    </LapisProvider>
   );
 }
 
